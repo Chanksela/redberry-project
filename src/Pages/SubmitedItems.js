086021d33237da9b52500ba68333d713cb8f06a9
chanksela <kakhachankseliani@gmail.com>
2022-03-10 02:16:57 +0400

    succesfully fetching posted applications, but displaying them is a little problem

--- src/Pages/SubmitedItems.js
+++ src/Pages/SubmitedItems.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-const URL = "https://bootcamp-2022.devtest.ge/api/applications";
 const token = "8b3c3554-eef2-4147-98b5-e4694c656950";
+const URL = `https://bootcamp-2022.devtest.ge/api/applications?token=${token}`;
 const authAxios = axios.create({
   baseURL: URL,
   headers: { Authorization: `Bearer ${token}` },
 });
+
 function SubmitedItems() {
+  const [fetchedApplications, setFetchedApplications] = useState("");
   const fetchedApps = () => {
     authAxios
       .get(URL)
-      .then((response) => console.log(response.data))
-      .then((apps) => console.log(apps));
+      .then((response) => response.data)
+      .then((apps) => setFetchedApplications(apps));
   };
+  useEffect(() => {
+    fetchedApps();
+  });
   return (
-    <>
-      <button onClick={fetchedApps}>Show Applications</button>
+    <div>
+      <button onClick={fetchedApps}>Show Feched Applications</button>
       <Link to="/">Main Page</Link>
-    </>
+    </div>
   );
 }
 
